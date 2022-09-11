0ad73e9c2c5de96adbdff8b089c6de4a4356b96e
revazashvili <levanrevazashvili@gmail.com>
2022-09-11 18:57:32 +0400

    setup warp server

--- warp/src/main.rs
+++ warp/src/main.rs
@@ -1,9 +1,16 @@
+use crate::{db::init_db, routes::customer_routes};
+
 mod db;
 mod handlers;
 mod models;
 mod routes;
 
-fn main() {
-    println!("Before Hello World!");
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let db = init_db();
+    let customer_routes = customer_routes(db);
+
+    warp::serve(customer_routes)
+        .run(([127, 0, 0, 1], 3000))
+        .await
 }
